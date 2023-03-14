1a9f49eece430d4245dc6e965fb8b8dfa69c5416
ahmadou dia <dia.ahmadou23@gmail.com>
2023-03-14 13:45:34 +0100

    ADD: ClearDB connection

--- util/database.js
+++ util/database.js
@@ -1,9 +1,21 @@
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize('cookbook-node', 'root', ')w3x9KN@@', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+
+// module.exports = sequelize;
+
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('cookbook-node', 'root', ')w3x9KN@@', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+  dialect: "mysql",
+  protocol: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: "Amazon RDS",
+  },
 });
-
 
 module.exports = sequelize;
