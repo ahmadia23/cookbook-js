a219339ebcb926ce54e85e1a307f8e32e62e7120
ahmadou dia <dia.ahmadou23@gmail.com>
2023-03-16 12:16:41 +0100

    TEST: new heroko configs

--- util/database.js
+++ util/database.js
@@ -5,13 +5,12 @@ const Sequelize = require("sequelize");
 console.log(process.env);
 
 const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,
-  process.env.MYSQL_USERNAME,
-  process.env.MYSQL_PASSWORD,
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
   {
     dialect: "mysql",
     protocol: "mysql",
-    socketPath: "/var/run/mysqld/mysqld.sock",
     logging: false,
     dialectOptions: {
       ssl: {
@@ -20,5 +19,20 @@ const sequelize = new Sequelize(
     },
   }
 );
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_DATABASE,
+//   process.env.MYSQL_USERNAME,
+//   process.env.MYSQL_PASSWORD,
+//   {
+//     dialect: "mysql",
+//     protocol: "mysql",
+//     logging: false,
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+// );
 
 module.exports = sequelize;
